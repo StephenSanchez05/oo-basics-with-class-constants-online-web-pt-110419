3e00d7419a1091d79b99b36960c4025b6368f41d
Stephen Sanchez <stephensanchez05@gmail.com>
2019-12-01 01:25:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS != brand
-      BRANDS << brand
-      BRANDS.uniq
+    BRANDS.each do |compare|
+      if brand != compare
+        BRANDS << brand
     end
   end
 
