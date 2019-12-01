f8b5466178127e54ad9f6c6b5410f0119973a5ad
Stephen Sanchez <stephensanchez05@gmail.com>
2019-12-01 01:33:57 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  BRANDS = []
+  BRAND = []
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand 
+    BRAND << brand 
   end
 
   def cobble
@@ -14,6 +14,6 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  BRANDS = BRANDS.uniq
+  BRANDS = BRAND.uniq
   
 end
